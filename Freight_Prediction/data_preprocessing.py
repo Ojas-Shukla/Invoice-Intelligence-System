d63c7b93b4a0dc17e1eaa93bdaa45d9a601d9206
Ojas-Shukla <ojasshukla2007@gmail.com>
2026-06-08 15:52:44 +0530

    Add files via upload

--- Freight_Prediction/data_preprocessing.py
+++ Freight_Prediction/data_preprocessing.py
@@ -1,1 +1,19 @@
+import sqlite3
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
+def load_vendor_invoice(db_path: str):
+    conn = sqlite3.connect(db_path)
+    query = "SELECT * FROM vendor_invoice"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+
+def prepare_features(df : pd.DataFrame):
+    X = df[["Dollars", "Quantity"]]
+    y = df["Freight"]
+    return X,y
+
+def split_data(X,y, test_size=0.2, random_state=42):
+
+    return train_test_split(X,y,test_size=test_size,random_state=random_state)
